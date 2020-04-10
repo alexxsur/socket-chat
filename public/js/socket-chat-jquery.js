ca2aff8818f6b7a08dff8ddd2602248b46640cc0
Alex <alexxsur@gmail.com>
2020-04-10 17:40:44 -0500

    Enviar y renderizar mensajes

--- public/js/socket-chat-jquery.js
+++ public/js/socket-chat-jquery.js
@@ -1,7 +1,13 @@
 var params = new URLSearchParams(window.location.search);
 
+var nombre = params.get('nombre');
+var sala = params.get('sala');
+
 // Referencias de jQuery
 var divUsuarios = $('#divUsuarios');
+var formEnviar = $('#formEnviar');
+var txtMensaje = $('#txtMensaje');
+var divChatbox = $('#divChatbox');
 
 // Funciones para renderizar usuarios
 
@@ -22,6 +28,20 @@ function renderizarUsuarios(personas) {
     divUsuarios.html(html);
 }
 
+function renderizarMensajes(mensaje) {
+    var html = '';
+    html += '<li class= "animated fadeIn">';
+    html += '<div class="chat-img"><img src="assets/images/users/1.jpg" alt="user" /></div>';
+    html += '<div class="chat-content">';
+    html += '    <h5>' + mensaje.nombre + '</h5>';
+    html += '    <div class="box bg-light-info">' + mensaje.mensaje + '</div>';
+    html += '</div>';
+    html += '<div class="chat-time">10:56 am</div>';
+    html += '</li>';
+
+    divChatbox.append(html);
+}
+
 // Listeners
 
 divUsuarios.on('click', 'a', function() {
@@ -29,4 +49,22 @@ divUsuarios.on('click', 'a', function() {
     if (id) {
         console.log(id);
     }
+})
+
+formEnviar.on('submit', function(e) {
+    e.preventDefault();
+    console.log(txtMensaje.val());
+    if (txtMensaje.val().trim().length === 0) {
+        return;
+    }
+
+    // Enviar información
+    socket.emit('crearMensaje', {
+        nombre: nombre,
+        mensaje: txtMensaje.val()
+    }, function(mensaje) {
+        txtMensaje.val('').focus();
+        renderizarMensajes(mensaje);
+    });
+
 })
